5c19dca094b35f5e1ca6c5694acee7480eb7540e
FarazAzeemi <58322475+FarazAzeemi@users.noreply.github.com>
2019-12-10 21:05:52 -0800

    Delete dict.py

--- dict.py
+++
@@ -1,8 +0,0 @@
-fn=input("Enter your first name= ")
-ln=input("enter your last name = ")
-choice1=int(input("enter your choice = "))
-name = {choice1: fn}
-print(name[choice1])
-choice2=int(input("enter your choice = "))
-name1 = {choice2: ln}
-print(name1[choice2])
