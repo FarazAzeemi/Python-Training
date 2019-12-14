e15c595e305f7db2e7f2201da8d9d7f9765e5162
FarazAzeemi <58322475+FarazAzeemi@users.noreply.github.com>
2019-12-14 04:53:37 -0800

    Add files via upload

--- primenumbers.py
+++ primenumbers.py
@@ -1,14 +1,13 @@
 def prime_numbers():
-    n = int(input("Enter any number :"))
-    if n > 1 and n == 2:
-        print(n, "is prime number")
-        yield n
-    if n % n == 0 and n % 2 != 0:
-        print(n, " is a prime number")
-        yield n
-    elif n > 2 and n % 2 == 0:
-        print(n, "It is not a prime number")
-        yield n
-prime_numbers()
-next(prime_numbers())
+  num = int(input("Enter the number"))
 
+
+  if num > 1:
+
+    for i in range(2, num):
+      if (num % i) == 0:
+        print(num, "is not a prime number")
+        break
+    else:
+      print(num, "is a prime number")
+prime_numbers()
